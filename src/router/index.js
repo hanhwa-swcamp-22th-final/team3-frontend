c9527fb12f5366d11ecb35762c6b138f7da32104
LimRaynor <morden9137@gmail.com>
2026-03-17 17:57:43 +0900

    router admin/kms 연결

--- src/router/index.js
+++ src/router/index.js
@@ -69,7 +69,7 @@ const routes = [
             {
                 path: 'knowledge-hub',
                 name: 'AdminKnowledgeHub',
-                component: Placeholder,
+                component: () => import('@/views/admin/AdminKnowledgeHub.vue'),
             },
         ],
     },
