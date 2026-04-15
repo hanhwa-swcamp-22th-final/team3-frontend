48cba783752e057ed6466aed1f4cd30d5adc7315
rlatlsdn98 <rlatlsdn98@naver.com>
2026-04-15 11:41:23 +0900

    안쓰는 mock data 파일 정리

--- src/mocks/teamleader/index.js
+++ src/mocks/teamleader/index.js
@@ -1,12 +1,1 @@
-﻿export * from './dashboard'
-export * from './kpiReport'
-export * from './aiEvaluation'
-export * from './notification'
-export * from './noticeBoard'
-export * from './scmOrders'
-export * from './ocsaScore'
-export * from './knowledgeApproval'
 export * from './knowledgeHub'
-export * from './facilityStatus'
-export * from './taskMatching'
-
